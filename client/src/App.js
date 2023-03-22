bcd266574b7a8ece463405d7d661ccdfda1cf5bc
Lipon Roy <liponbdbd@gmail.com>
2023-03-22 14:40:22 +0600

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Hero from './components/hero/Hero';
+
+function App() {
+	return (
+		<>
+			<Navbar></Navbar>
+			<Hero></Hero>
+		</>
+	);
+}
+
+export default App;
